6acbdca043c90f3522200fc8c81368273553d02e
Melih Odabas <melih_odabas@hotmail.com>
2025-01-16 23:57:12 +0300

    - update deps

--- src/Modules/SecondService/ModularMonolith.Modules.SecondService/Features/Stores/Orleans/StoreGrain.cs
+++ src/Modules/SecondService/ModularMonolith.Modules.SecondService/Features/Stores/Orleans/StoreGrain.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ModResults;
-using ModularMonolith.Modules.FirstService.IntegrationContracts.Integrations.Books;
 using ModularMonolith.Modules.SecondService.Data;
 using ModularMonolith.Modules.SecondService.IntegrationContracts.Integrations.Stores;
 using ModularMonolith.Shared.Data.SimpleOutbox.Extensions;
