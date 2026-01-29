91225ad1fb5f06955dce8b3bfe96ac4618b18519
Melih Odabas <melih_odabas@hotmail.com>
2026-01-29 11:09:10 +0300

    - update deps to latest - target .net 10

--- src/Shared/ModularMonolith.Shared/Guids/GuidV7.cs
+++ src/Shared/ModularMonolith.Shared/Guids/GuidV7.cs
@@ -1,11 +1,9 @@
-﻿using UUIDNext;
-
-namespace ModularMonolith.Shared.Guids;
+﻿namespace ModularMonolith.Shared.Guids;
 
 public static class GuidV7
 {
   public static Guid CreateVersion7()
   {
-    return Uuid.NewDatabaseFriendly(Database.PostgreSql);
+    return Guid.CreateVersion7();
   }
 }
